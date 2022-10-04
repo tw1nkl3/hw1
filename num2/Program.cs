6c1ab452de34cd7f74ee600e498877ed6cb81d16
Ivan <grabovivan@gmail.com>
2022-10-04 21:51:44 +0300

    HW

---
+++ num2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a)
+{
+    max = b;
+}
+Console.WriteLine("max: " + max);
---
+++ num4/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Clear();
+Console.Write("Введите три числа по очереди: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[3] {num1, num2, num3};
+Console.WriteLine("max: " + array.Max());
